a7c4e33a3e50de0ce61c284f955f4cac6932792a
Jerry Kiche <jerrykiche61@gmail.com>
2023-06-18 13:50:14 -0700

    Fix error trial 4

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- * main - function to print a sentence
+ * main - Entry point 
  * Return : 0 always (success)
-*/
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
