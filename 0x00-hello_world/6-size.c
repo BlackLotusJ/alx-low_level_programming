349994384fd78aac14c7cc1344567fcc952be0ce
Jerry Kiche <jerrykiche61@gmail.com>
2023-06-18 14:23:40 -0700

    Creared 6-size.c and checked with betty

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: always 0 (success)
+*/
+int main(void)
+{
+	printf("Size of a char: %d byte(s)", sizeof(char));
+	printf("Size of an int: %d byte(s)", sizeof(int));
+	printf("Size of a long int: %d byte(s)", sizeof(lon int));
+	printf("Size of a long long int %d byte(s)", sizeof(long long int));
+	printf("Size of a float: %d byte(s)", sizeof(float));
+	return (0);
+}
