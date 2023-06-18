162f53b8588a2d3618decf3fdaf5518db2be3808
Jerry Kiche <jerrykiche61@gmail.com>
2023-06-18 13:38:02 -0700

    5-printf.c was created

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/*
+ * main - function to print a sentence
+ * return : always will return 0
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
