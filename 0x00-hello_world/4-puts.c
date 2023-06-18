573cfab13942fa0c5f83e2ffa0c2253495a60963
Jerry Kiche <jerrykiche61@gmail.com>
2023-06-18 13:30:21 -0700

    Made c file and checked with betty

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main-Function to output using puts
+ * retun : always 0
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
