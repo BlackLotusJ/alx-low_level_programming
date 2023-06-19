784f75c583b25f41b113da2cf390f41ddbce120a
Jerry Kiche <jerrykiche61@gmail.com>
2023-06-19 16:42:31 -0700

    Created 5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: Output all single digit base 10 numbers
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int b10;
+
+	for (b10 = 0 ; b10 < 10 ; b10++)
+	{
+		printf("%d\n", b10);
+	}
+	return (0);
+}
