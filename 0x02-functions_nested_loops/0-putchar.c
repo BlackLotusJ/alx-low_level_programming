638cfa944ead3ce74bb35b423cbabf0958e070be
Jerry Kiche <jerrykiche61@gmail.com>
2023-06-20 10:36:21 -0700

    Fixed error in output

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "main.h"
 /**
  * main - Entry point
  * Description: Function to output _putchar
@@ -6,6 +7,13 @@
  */
 int main(void)
 {
-	printf("_putchar");
+	char ch1[8] = "_putchar";
+	int chpos;
+
+	for (chpos = 0 ; chpos <= 7 ; chpos++)
+	{
+		_putchar(ch1[chpos]);
+	}
+	_putchar('\n');
 	return (0);
 }
