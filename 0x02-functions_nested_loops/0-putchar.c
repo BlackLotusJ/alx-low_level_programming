af463dcb48fbad8e5c1f49f8a8e789ab97d87141
Jerry Kiche <jerrykiche61@gmail.com>
2023-06-20 10:15:31 -0700

    Creayted 0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: Function to output _putchar
+ * Return: ALways 0 (success)
+ */
+int main(void)
+{
+	printf("_putchar");
+	return (0);
+}
