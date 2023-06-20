94d3666cb904d81e22cf7f3f923d48f38be74591
Jerry Kiche <jerrykiche61@gmail.com>
2023-06-20 11:35:55 -0700

    Created 2-alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_alphabet_x10 - a function that prints 10 times the alphabet
+ *
+ * Return: void type
+ */
+void print_alphabet_x10(void)
+{
+	int alphaCount = 0;
+
+	for (; alphaCount < 10; alphaCount++)
+	{
+		char alpha = 'a';
+
+		for (; alpha <= 'z'; alpha++)
+			_putchar(alpha);
+
+		_putchar('\n');
+	}
+	return;
+}
