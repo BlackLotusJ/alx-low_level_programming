f4b4e78607db0ac277c76484d2cba60e21543062
Jerry Kiche <jerrykiche61@gmail.com>
2023-06-21 07:20:03 -0700

    Added 8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * jack_bauer - Entry point
+ * Description: Function to print every minutre of jack Bauer
+ * Return: void data type
+ */
+void jack_bauer(void)
+{
+	int hour = 0, min = 0;
+
+	while (hour < 24)
+	{
+		_putchar('0' + (hour / 10));
+		_putchar('0' + (hour % 10));
+		_putchar(':');
+
+		while (min < 60)
+		{
+			_putchar('0' + (min / 10));
+			_putchar('0' + (min % 10));
+			_putchar('\n');
+			min++;
+		}
+		min = 0;
+		hour++;
+	}
+}
