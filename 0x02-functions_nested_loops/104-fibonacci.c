3e7343ff9c1e7e7bd1033d854dd2f9fb544725f9
Jerry Kiche <jerrykiche61@gmail.com>
2023-06-21 12:19:58 -0700

    Added 104-fibonacci.c

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - prints the first 98 Fibonacci numbers
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	unsigned long int n1 = 0;
+	unsigned long int n2 = 1;
+	unsigned long int n3 = 0;
+	unsigned long total = 0;
+
+	while (total < 99)
+	{
+		n3 = n1 + n2;
+		printf("%lu", n3);
+		putchar(',');
+		putchar(' ');
+		n1 = n2;
+		n2 = n3;
+		total++;
+	}
+	putchar('\n');
+	return (0);
+}
