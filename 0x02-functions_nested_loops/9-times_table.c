8465adc4d81f7912efed36343556340ecbdf5a6e
Jerry Kiche <jerrykiche61@gmail.com>
2023-06-21 07:51:01 -0700

    9-times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,40 @@
+#include "main.h"
+/**
+ * times_table - print multiplication table
+ *
+ * Return: void data type
+ */
+void times_table(void)
+{
+	int num1;
+	int num2;
+	int product;
+
+	for (num1 = 0; num1 <= 9; num1++)
+	{
+		for (num2 = 0; num2 <= 9; num2++)
+		{
+			product = (num1 * num2);
+
+			if (num2 == 0)
+			{
+				_putchar('0' + product);
+			}
+			else if (product <= 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar('0' + product);
+			}
+			else if (product > 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar('0' + (product / 10));
+				_putchar('0' + (product % 10));
+			}
+		}
+		_putchar('\n');
+	}
+}
