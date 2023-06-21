cbcaa4e73a274277f943ca0112c4213f8ef0d68a
Jerry Kiche <jerrykiche61@gmail.com>
2023-06-21 12:45:28 -0700

    Git push

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,54 +1,20 @@
-#include "main.h"
+#include <stdio.h>
+
 /**
- * print_times_table - function that prints the n times table, starting with 0.
- * @n: integer n
+ * print_times_table - Prints the n times table, starting with 0
+ * @n: The number for which the times table is generated
+ *
+ * Description: This function prints the times table for the given number,
+ * starting with 0. It prints the table up to 10 times the given number.
+ * If n is not in the range of 0 to 15 (inclusive), nothing is printed.
  * Return: void data type
  */
 void print_times_table(int n)
 {
-	int num1;
-	int num2;
-	int product;
+	if (n < 0 || n > 15)
+		return;  /* Do nothing if n is out of range */
 
-	if (n >= 0 && n <= 15)
-	{
-		for (num1 = 0; num1 <= n; num1++)
-		{
-			for (num2 = 0; num2 <= n; num2++)
-			{
-				product = (num1 * num2);
+	for (int i = 0; i <= 10; ++i)
+		printf("%d x %d = %d\n", i, n, i * n);
+}
 
-				if (num2 == 0)
-				{
-					_putchar('0' + product);
-				}
-				else
-				{
-					_putchar(',');
-					_putchar(' ');
-				}
-				
-				if (product <= 9)
-				{
-					_putchar(' ');
-					_putchar(' ');
-					_putchar('0' + product);
-				}
-				else if (product > 9 && product < 100)
-				{
-					_putchar(' ');
-					_putchar('0' + (product / 10));
-					_putchar('0' + (product % 10));
-				}
-				else if (product >= 100)
-				{
-					_putchar('0' + (product / 100));
-					_putchar('0' + ((product / 10) % 10));
-					_putchar('0' + (product % 10));
-				}
-			}
-		}
-		_putchar('\n');
-	}
-}
-}
