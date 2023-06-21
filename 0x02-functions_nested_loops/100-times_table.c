0536f8b42f602f217549d19221580c4615c79fff
Jerry Kiche <jerrykiche61@gmail.com>
2023-06-21 13:00:47 -0700

    Fix length

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
  * print_times_table - Prints the n times table, starting with 0
@@ -7,14 +7,43 @@
  * Description: This function prints the times table for the given number,
  * starting with 0. It prints the table up to 10 times the given number.
  * If n is not in the range of 0 to 15 (inclusive), nothing is printed.
- * Return: void data type
  */
 void print_times_table(int n)
 {
-	if (n < 0 || n > 15)
-		return;  /* Do nothing if n is out of range */
+	int i, j, product;
 
-	for (int i = 0; i <= 10; ++i)
-		printf("%d x %d = %d\n", i, n, i * n);
+	if (n < 0 || n > 15)
+		return;
+
+	for (i = 0; i <= 10; ++i)
+	{
+		_putchar('0');
+		for (j = 1; j <= 10; ++j)
+		{
+			_putchar(',');
+			_putchar(' ');
+
+			product = i * j;
+
+			if (product < 10)
+			{
+				_putchar(' ');
+				_putchar(' ');
+			}
+			else if (product < 100)
+				_putchar(' ');
+
+			if (product >= 100)
+			{
+				_putchar((product / 100) + '0');
+				_putchar(((product / 10) % 10) + '0');
+			}
+			else
+				_putchar((product / 10) + '0');
+
+			_putchar((product % 10) + '0');
+		}
+		_putchar('\n');
+	}
 }
 
