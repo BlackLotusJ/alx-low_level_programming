04c04146e846f344b9d2bc5a4374592dfc4252bf
Jerry Kiche <jerrykiche61@gmail.com>
2023-06-22 14:00:48 -0700

    Fixed betty error in positive_or_negative file

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,5 +1,10 @@
 #include "main.h"
-
+/**
+ * positive_or_negative - Entry point
+ * @i: integer i
+ * Description: Find positive and negative
+ * Return: no return value
+ */
 void positive_or_negative(int i)
 {
 	if (i > 0)
