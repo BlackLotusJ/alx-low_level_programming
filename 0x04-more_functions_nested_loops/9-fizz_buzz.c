3773dbb347276a0e39eb7e0db768a35c89940c6f
Jerry Kiche <jerrykiche61@gmail.com>
2023-06-23 15:08:34 -0700

    Fixed error in fizz buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,31 +1,30 @@
 #include <stdio.h>
+#include "main.h"
 /**
- * main - print 1 to 100,
- * multiples of 3 with Fizz,
- * multiples of 5 with Buzz
- * and multiple of both 3 and 5 with FizzBuzz
- * Return: Always 0
+ * main - entry point
+ *
+ * Description: prints the numbers 1 - 100 w/
+ * Fizz for multiples of 3, Buzz for multiples of 5,
+ * and FizzBuzz for multiples of both
+ *
+ * Return: always 0
  */
 int main(void)
 {
-	for (int i = 1; i <= 100; i++)
+	int i;
+
+	for (i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0)
-		{
+		if (i % 15 == 0)
+			printf("FizzBuzz");
+		else if (i % 3 == 0)
 			printf("Fizz");
-		}
-		if (i % 5 == 0)
-		{
+		else if (i % 5 == 0)
 			printf("Buzz");
-		}
-		if (i % 3 != 0 && i % 5 != 0)
-		{
-			printf("%d", i);
-		}
+		else
+			printf("%i", i);
 		if (i < 100)
-		{
 			printf(" ");
-		}
 	}
 	printf("\n");
 	return (0);
