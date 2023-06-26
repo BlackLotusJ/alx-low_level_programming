159c5927bd4f369c18fef693dabc34d596d2bcab
Jerry Kiche <jerrykiche61@gmail.com>
2023-06-26 14:47:18 -0700

    Function to print every other number of a string

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * _strlen - returns the length of a string
+ * @s: string
+ * Return: length
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+	return (len);
+}
+
+/**
+ * puts2 - prints every other character of a string,
+ * starting with the first character
+ * @str: string to print
+ */
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0' && i < _strlen(str); i += 2)
+		_putchar(str[i]);
+	_putchar('\n');
+}
