d5e6fe40443b486e07b6d3638f45c8bcc68cdad0
Jerry Kiche <jerrykiche61@gmail.com>
2023-06-27 07:09:38 -0700

    Fix 80 column betty error

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,7 +8,7 @@
 int _atoi(char *s)
 {
 	int sign = 1;
-	int result = 0;
+	int num = 0;
 	int i = 0;
 
 	/* Skip leading whitespace */
@@ -26,7 +26,8 @@ int _atoi(char *s)
 	while (s[i] >= '0' && s[i] <= '9')
 	{
 		/* Check for overflow */
-		if (result > INT_MAX / 10 || (result == INT_MAX / 10 && (s[i] - '0') > INT_MAX % 10))
+		if (num > INT_MAX / 10 ||
+				(num == INT_MAX / 10 && (s[i] - '0') > INT_MAX % 10))
 		{
 			if (sign == 1)
 				return (INT_MAX);
@@ -34,10 +35,9 @@ int _atoi(char *s)
 				return (INT_MIN);
 		}
 
-		result = result * 10 + (s[i] - '0');
+		num = num * 10 + (s[i] - '0');
 		i++;
 	}
 
-	return (sign * result);
+	return (sign * num);
 }
-
