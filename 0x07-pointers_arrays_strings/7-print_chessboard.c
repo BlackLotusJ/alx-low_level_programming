c356cfab7cc67bffe164d01be709ab53c96cdd34
Jerry Kiche <jerrykiche61@gmail.com>
2023-07-03 12:42:13 -0700

    Fix error trial 1

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,17 +1,17 @@
 #include "main.h"
-#include <stdio.h>
 /**
  * print_chessboard - a function that prints the chessboard
  * @a: 2 Dimensional array
  */
 void print_chessboard(char (*a)[8])
 {
-	for (int i = 0; i < 8; i++)
+	int row;
+	int column;
+
+	for (row = 0; row < 8; row++)
 	{
-		for (int j = 0; j < 8; j++)
-		{
-			printf("%c ", a[i][j]);
-		}
-		printf("\n");
+		for (column = 0; column < 8; column++)
+			_putchar(a[row][column]);
+		_putchar('\n');
 	}
 }
