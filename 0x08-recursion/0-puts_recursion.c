1a76f53aafae7ab65978c3fad0b8086dca73d257
Jerry Kiche <jerrykiche61@gmail.com>
2023-07-05 12:06:38 -0700

    Fix compile error trial 1

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,9 +6,14 @@
  */
 void _puts_recursion(char *s)
 {
-	if (*s == '\0')
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		s++;
+		_puts_recursion(s);
+	}
+	else
+	{
 		_putchar('\n');
-		return;
-	_putchar(*s);
-	_puts_recursion(s + 1);
+	}
 }
