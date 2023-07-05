6a9e47ff0a0475d7e37029889656ec6c309b84b9
Jerry Kiche <jerrykiche61@gmail.com>
2023-07-05 12:34:52 -0700

    Function to find tje factorial given an integer

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * factorial - Function to print factorial
+ * @n: factorial to be found
+ */
+int factorial(int n)
+{
+	if (n == 0)
+		return (0);
+	else if (n == 1)
+		return (1);
+	else
+		return (n * factorial(n - 1));
+
+}
