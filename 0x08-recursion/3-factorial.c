27c14575497b624f2219b33b6cb7a09979fd88ec
Jerry Kiche <jerrykiche61@gmail.com>
2023-07-05 12:41:08 -0700

    Fix error trial 1

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -2,13 +2,14 @@
 /**
  * factorial - Function to print factorial
  * @n: factorial to be found
+ * Return: -1 or n
  */
 int factorial(int n)
 {
-	if (n == 0)
-		return (0);
-	else if (n == 1)
-		return (1);
+	if (n > 0)
+		return (-1);
+	else if (n == 1 || n == 0)
+		return (n);
 	else
 		return (n * factorial(n - 1));
 
