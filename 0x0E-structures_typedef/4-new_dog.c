78b6bcb09bb7768975cfd1a44263a1925a40da5a
Jerry Kiche <jerrykiche61@gmail.com>
2023-07-17 13:07:38 -0700

    Fixed betty error

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -10,7 +10,7 @@
 int _strlen(const char *str)
 {
 	int length = 0;
-	
+
 	while (*str++)
 		length++;
 	return (length);
@@ -26,6 +26,7 @@ int _strlen(const char *str)
 char *_strcopy(char *dest, char *src)
 {
 	int i;
+
 	for (i = 0; src[i]; i++)
 		dest[i] = src[i];
 	dest[i] = '\0';
