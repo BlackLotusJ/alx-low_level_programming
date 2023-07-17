5c9dcaae39d2620e9dc2d327b238a3a9c402d47b
Jerry Kiche <jerrykiche61@gmail.com>
2023-07-17 12:44:05 -0700

    Fix error trial 1

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,15 +2,27 @@
 #define DOG_H
 /**
  * struct dog - dog structure
- * @name: dog's name
- * @age: age of dog
- * @owner: dog ownwer
- */
+ *
+ * @name: name of dog
+ * @age: age of the dog
+ * @owner: dog owner
+ *
+*/
 struct dog
 {
 	char *name;
-	int age;
+	float age;
 	char *owner;
-}
+};
+
+/**
+ * dog_t -Typedef for struct dog
+*/
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif
