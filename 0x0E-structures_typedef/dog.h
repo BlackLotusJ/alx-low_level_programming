438dccb2334f736c0823a926f41e88dcdf5e3aa4
Jerry Kiche <jerrykiche61@gmail.com>
2023-07-17 12:29:31 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,14 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - dog structure
+ * @name: dog's name
+ * @age: age of dog
+ * @owner: dog ownwer
+ */
+struct dog
+{
+	char *name;
+	int age;
+	char *owner;
+}
