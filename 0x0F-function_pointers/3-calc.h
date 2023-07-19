6272127dbddc7dad4b2efae4e342cca972b9737d
Jerry Kiche <jerrykiche61@gmail.com>
2023-07-19 15:45:25 -0700

    Trial 5

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,8 +1,8 @@
-#ifndef _CALC_H
-#define _CALC_H
-
+#ifndef _CALC_H_
+#define _CALC_H_
 /**
  * struct op - Struct op
+ *
  * @op: The operator
  * @f: The function associated
  */
@@ -19,4 +19,4 @@ int op_div(int a, int b);
 int op_mod(int a, int b);
 int (*get_op_func(char *s))(int, int);
 
-#endif
+#endif /* _CALC_H_ */
