2aea1ab2b4905c06ca81efc587180131d20ced23
Jerry Kiche <jerrykiche61@gmail.com>
2023-07-19 15:33:34 -0700

    Fix 3

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,29 +1,16 @@
-#include "3-calc.h"
 #include <stdio.h>
-#include <string.h> /*for atoi*/
-#include <stdlib.h> /*for exit*/
-
+#include <stdlib.h>
+#include "3-calc.h"
 /**
- * main - Program Entry
+ * main - performs simple operations
+ * @argc: number of arguments passed
+ * @argv: array of pointers to arguments
  *
- * Description: Program that takes 4 arguments where 2 are
- *              int variables plus an operator to perform
- *              a function between the two numbers
- *
- *              Usage: ./calc num1 operator num2
- *                         |     |       |     |_ argv[3]
- *                         |     |       |_ argv[2]
- *                         |     |_ argv[1]
- *                         |_ argv[0]
- *
- * @argc: argument counter
- * @argv: argument vector
- *
- * Return: 0-on success
+ * Return: always 0
  */
 int main(int argc, char *argv[])
 {
-	int a, b ,c;
+	int a, b, c;
 	int (*f)(int, int);
 
 	if (argc != 4)
@@ -32,12 +19,12 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 	a = atoi(argv[1]);
-	b = atoi (argv[3]);
+	b = atoi(argv[3]);
 	f = get_op_func(argv[2]);
 	if (f == NULL)
 	{
 		printf("Error\n");
-		prinntf(99);
+		exit(99);
 	}
 	c = f(a, b);
 	printf("%d\n", c);
--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,31 +1,72 @@
 #include "3-calc.h"
-#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
 /**
- * get_op_func - get ops function pointer of type char array
- *               that accepts two inputs of int data type
+ * op_add - adds a and b
+ * @a: first number
+ * @b: second number
  *
- * @s: a character pointer pointing to a symbol from the program argument
- *
- * Return: one of the operator functions to perform calculations
+ * Return: sum of a and b
  */
-int (*get_op_func(char *s))(int, int)
+int op_add(int a, int b)
 {
-	/* struct opts of struct op_t */
-	op_t ops[] = {
-		{"+", op_add},
-		{"-", op_sub},
-		{"*", op_mul},
-		{"/", op_div},
-		{"%", op_mod},
-		{NULL, NULL}
-	};
-	int i = 0;
+	return (a + b);
+}
 
-	while (i < 5)
+/**
+ * op_sub - subtracts b from a
+ * @a: first number
+ * @b: second number
+ *
+ * Return: difference of a and b
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiplies a and b
+ * @a: first number
+ * @b: second number
+ *
+ * Return: multiplication of a and b
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - divides a by b
+ * @a: first number
+ * @b: second number
+ *
+ * Return: integer division of a by b
+ */
+int op_div(int a, int b)
+{
+	if (b == 0)
 	{
-		if (*s == *ops[i].op)
-			return (ops[i].f);
-		i++;
+		printf("Error\n");
+		exit(100);
 	}
-	return (NULL);
+	return (a / b);
+}
+
+/**
+ * op_mod - find the remainder of the division of a by b
+ * @a: first number
+ * @b: second number
+ *
+ * Return: remainder of the division of a by b
+ */
+int op_mod(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a % b);
 }
