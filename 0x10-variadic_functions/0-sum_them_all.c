27fbbe4ad16dc75a9c4adb7caed34b53ed46cc0b
Jerry Kiche <jerrykiche61@gmail.com>
2023-07-20 14:20:20 -0700

    Changed data type

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -14,7 +14,7 @@ int sum_them_all(const unsigned int n, ...)
 
 	for (unsigned int i = 0; i < n; i++)
 	{
-		sum += va_arg(ptr, int);
+		sum += va_arg(ptr, unsigned int);
 	}
 	va_end(ptr);
 	return (sum);
