f8b10f5c21138aa38faa5eedb48bcb4dd2207985
Jerry Kiche <jerrykiche61@gmail.com>
2023-07-20 14:08:09 -0700

    Fixed betty error

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -8,10 +8,11 @@
 int sum_them_all(const unsigned int n, ...)
 {
 	int sum = 0;
-
 	va_list ptr;
+
 	va_start(ptr, n);
-	for (int i = 0;i < n; i++)
+
+	for (int i = 0; i < n; i++)
 	{
 		sum += va_arg(ptr, int);
 		va_end(ptr);
