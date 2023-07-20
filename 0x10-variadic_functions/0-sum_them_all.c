71c0c47739dfad8cc14522770593818e39a418ee
Jerry Kiche <jerrykiche61@gmail.com>
2023-07-20 14:17:02 -0700

    Trial 2

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -15,8 +15,7 @@ int sum_them_all(const unsigned int n, ...)
 	for (unsigned int i = 0; i < n; i++)
 	{
 		sum += va_arg(ptr, int);
-		va_end(ptr);
-		if (n == 0) return (0);
-		else return (sum);
 	}
+	va_end(ptr);
+	return (sum);
 }
