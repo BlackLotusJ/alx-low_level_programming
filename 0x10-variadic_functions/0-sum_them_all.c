c6766bfc9a229749b77f67403db74776c8dc0b81
Jerry Kiche <jerrykiche61@gmail.com>
2023-07-20 14:14:29 -0700

    Fix compile error trial 1

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -7,12 +7,12 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
-	int sum = 0;
+	unsigned int sum = 0;
 	va_list ptr;
 
 	va_start(ptr, n);
 
-	for (int i = 0; i < n; i++)
+	for (unsigned int i = 0; i < n; i++)
 	{
 		sum += va_arg(ptr, int);
 		va_end(ptr);
