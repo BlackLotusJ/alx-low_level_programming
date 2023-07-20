7de1751bfcf1a9512b32628d86c7c94d57aaf57d
Jerry Kiche <jerrykiche61@gmail.com>
2023-07-20 14:24:27 -0700

    ADDED hader file

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -7,14 +7,14 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
-	unsigned int sum = 0;
+	int sum = 0;
 	va_list ptr;
 
 	va_start(ptr, n);
 
 	for (unsigned int i = 0; i < n; i++)
 	{
-		sum += va_arg(ptr, unsigned int);
+		sum += va_arg(ptr, int);
 	}
 	va_end(ptr);
 	return (sum);
