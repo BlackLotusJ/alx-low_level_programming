71d4c3ad800db50cb917fbb18733f0f88ebad98e
Jerry Kiche <jerrykiche61@gmail.com>
2023-07-20 14:05:17 -0700

    Created 0-sum_them_all

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdarg.h>
+/**
+ * sum_them_all -  variadic sum all integers
+ * @n: first argument
+ * Return: sum(int)
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	int sum = 0;
+
+	va_list ptr;
+	va_start(ptr, n);
+	for (int i = 0;i < n; i++)
+	{
+		sum += va_arg(ptr, int);
+		va_end(ptr);
+		return (sum);
+	}
+}
