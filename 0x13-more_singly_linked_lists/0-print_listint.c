292866b2415b88a3d2f89574274099cf15571f99
BlackLotusJ <jerrykiche61@gmail.com>
2023-08-01 21:14:29 +0300

    Fix error trial 2

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -3,15 +3,15 @@
  * print_listint_t - prints linked list elements
  * Return: number of elements
  */
-size_t print_list(const listint_t *h)
+size_t print_listint(const listint_t *h)
 {
 	const listint_t *current = h;
-	size_t count = 1;
+	size_t count = 0;
 
 	while (current != NULL)
 	{
-		printf("%lu",current -> n);
-		current = current -> next;
+		printf("%lu",current->n);
+		current = current->next;
 		count++;
 	}
 	return (count);
