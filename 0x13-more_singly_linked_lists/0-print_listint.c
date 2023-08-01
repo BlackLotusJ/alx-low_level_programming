7cf8ff36aceb8a878cc51211826085ae9bb18c06
BlackLotusJ <jerrykiche61@gmail.com>
2023-08-01 21:05:56 +0300

    updated

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * print_listint_t - prints linked list elements
+ * Return: number of elements
+ */
+size_t print_list(const listint_t *h)
+{
+	const listint_t *current = h;
+	size_t current;
+
+	while (current != NULL)
+	{
+		printf("%lu",current -> n);
+		current = current -> next;
+		count++;
+	}
+	return (count);
+}
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -5,15 +5,19 @@
 #include <stdlib.h>
 
 /**
- * listint_s - singly linked list
- * @n: node element
- * @next: poiter to next node
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * 
  */
 typedef struct listint_s
 {
-	int n;
-	struct listint_s *next;
-}listint_t;
+    int n;
+    struct listint_s *next;
+} listint_t;
 
 size_t print_listint(const listint_t *h);
+
 #endif/*_LISTS_H*/
