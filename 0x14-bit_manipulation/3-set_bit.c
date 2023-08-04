167e369cccf05662a03bfca17970bcd009494dad
BlackLotusJ <jerrykiche61@gmail.com>
2023-08-04 03:17:23 +0300

    Made changes

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -6,12 +6,15 @@
  * @index: index within binary number
  * Return: 1 if success, or -1 if error
  */
-int set_bit(unsigned long int *n, unsigned int index) {
-    if (index >= sizeof(unsigned long int) * 8) {
-        return -1; // Return -1 if the index is out of bounds for the data type
-    }
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int num = 1;
 
-    *n |= (1UL << index); // Set the bit at the given index to 1 using the OR bitwise operator
+	if (index > (sizeof(unsigned long int n) * 8))
+		return (-1);
 
-    return 1;
+	num = ~ (num << index);
+	*n = *n & num;
+	return (1);
+
 }
