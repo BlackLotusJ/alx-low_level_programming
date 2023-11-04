d48ef84dbf5737dfe998bad4a4cb5f0dbf980ba4
BlackLotusJ <jerrykiche61@gmail.com>
2023-11-05 00:16:05 +0300

    updated

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,14 +1,15 @@
 #include "hash_tables.h"
 
 /**
- * hash_djb2 - djb2 function
- * @str:  string 
- * Retrun: hash*/
+ * hash_djb2 - hash function to implement djb2 algorithm
+ * @str: string to hash  
+ * Retrun: hash index correspondind to the string
+ */
 unsigned long int hash_djb2(const unsigned char *str)
 {
 	unsigned long int hash;
 	int c;
-	
+
 	hash = 5381;
 	while ((c = *str++))
 	{
